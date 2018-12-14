6c0e75614dc74260d387a21ed136d2f32b7d08d9
Sebastian Isaza Gomez <sebastianisazagomez@gmail.com>
2018-12-14 10:49:48 -0500

    Log parse batch load

--- log-parse-console/src/main/java/com/log/parser/config/JpaConfig.java
+++ log-parse-console/src/main/java/com/log/parser/config/JpaConfig.java
@@ -2,9 +2,10 @@ package com.log.parser.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration
+@Configuration
 @EntityScan(basePackages = {"com.log.parser.common.entity"})
 @EnableJpaRepositories(basePackages = {"com.log.parser.dao.repo"})
 public class JpaConfig {
